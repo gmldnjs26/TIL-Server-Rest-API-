38330a2a1a56013cca1437ef861d5f2f73995fb5
gmldnjs26 <imleesky265@gmail.com>
2020-10-10 22:07:58 +0900

    TILMapper modify

--- src/main/java/com/example/gwak/dto/TodoList.java
+++ src/main/java/com/example/gwak/dto/TodoList.java
@@ -1,5 +1,16 @@
 package com.example.gwak.dto;
 
-public class TodoList {
+import java.util.Date;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TodoList {
+	private String id;
+	private String title;
+	private String contents;
+	private String createAt; // 만든 날짜
+	private Integer completed; // 0:Not Completed 1:Completed
 }
--- src/main/java/com/example/gwak/mapper/TodoListMapper.java
+++ src/main/java/com/example/gwak/mapper/TodoListMapper.java
@@ -1,5 +1,12 @@
 package com.example.gwak.mapper;
 
-public class TodoListMapper {
+import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+
+import com.example.gwak.dto.TodoList;
+
+@Mapper
+public interface TodoListMapper {
+	List<TodoList>getTIL(String id);	
 }
