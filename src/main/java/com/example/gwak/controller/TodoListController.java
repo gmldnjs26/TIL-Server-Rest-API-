54a0f35c4fc0bde689e465854c883d0b08d268cf
gmldnjs26 <imleesky265@gmail.com>
2020-10-14 23:46:24 +0900

    TodoList select insert update completed

--- src/main/java/com/example/gwak/controller/TodoListController.java
+++ src/main/java/com/example/gwak/controller/TodoListController.java
@@ -5,14 +5,41 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.gwak.dto.TodoList;
+import com.example.gwak.service.TodoListService;
+
+import lombok.RequiredArgsConstructor;
 
 @Controller
+@RequiredArgsConstructor
 public class TodoListController {
+	private final TodoListService todoListService;
 	
-	@GetMapping("/api/todolist")
-	public ResponseEntity<List<TodoList>> getTIL() {
-		return null;
+	@GetMapping("/api/selectTil")
+	public ResponseEntity<List<TodoList>> selectTil(String id) {
+		List<TodoList>todoList = todoListService.selectTil(id);
+		return ResponseEntity.ok(todoList);
+	}
+	
+	@PostMapping("/api/insertTil")
+	public ResponseEntity<String> insertTil(TodoList til) {
+		try {
+			todoListService.insertTil(til);
+			return ResponseEntity.ok("Success");
+		} catch(Exception e) {
+			return ResponseEntity.badRequest().build();
+		}
+	}
+	
+	@PostMapping("/api/updateTil")
+	public ResponseEntity<String> updateTil(TodoList til) {
+		try {
+			todoListService.updateTil(til);
+			return ResponseEntity.ok("Success");
+		} catch(Exception e) {
+			return ResponseEntity.badRequest().build();
+		}
 	}
 }
--- src/main/java/com/example/gwak/service/TodoListServiceImpl.java
+++ src/main/java/com/example/gwak/service/TodoListServiceImpl.java
@@ -2,25 +2,28 @@ package com.example.gwak.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.gwak.dto.TodoList;
+import com.example.gwak.mapper.TodoListMapper;
 
 @Service
 public class TodoListServiceImpl implements TodoListService{
+	@Autowired
+	private TodoListMapper todoListMapper;
+	
 	@Override
 	public List<TodoList> selectTil(String id) {
-		// TODO Auto-generated method stub
-		return null;
+		List<TodoList>todoList = todoListMapper.selectTil(id);
+		return todoList;
 	}
 	@Override
 	public void insertTil(TodoList til) {
-		// TODO Auto-generated method stub
-		
+		todoListMapper.insertTil(til);
 	}
 	@Override
 	public void updateTil(TodoList til) {
-		// TODO Auto-generated method stub
-		
+		todoListMapper.updateTil(til);
 	}
 }
