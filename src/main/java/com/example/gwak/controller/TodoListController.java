33c2ff03f2e55e29e85521c3b7b785b95e06f755
gmldnjs26 <imleesky265@gmail.com>
2020-10-22 23:34:41 +0900

    Sl4j Logger

--- src/main/java/com/example/gwak/controller/TodoListController.java
+++ src/main/java/com/example/gwak/controller/TodoListController.java
@@ -2,6 +2,8 @@ package com.example.gwak.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,17 +18,21 @@ import lombok.RequiredArgsConstructor;
 @Controller
 @RequiredArgsConstructor
 public class TodoListController {
+	
 	private final TodoListService todoListService;
+	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	
 	@GetMapping("/api/selectTil")
 	public ResponseEntity<List<TodoList>> selectTil(@RequestBody String id) {
+		logger.debug("==========================");
+		logger.debug("TodoListController execute");
 		List<TodoList>todoList = todoListService.selectTil(id);
 		return ResponseEntity.ok(todoList);
 	}
 	
 	@PostMapping("/api/insertTil")
 	public ResponseEntity<String> insertTil(@RequestBody TodoList todoList) {
-		System.out.println("InsertTil" + todoList.getId());
 		try {
 			todoListService.insertTil(todoList);
 			return ResponseEntity.ok("Success");
