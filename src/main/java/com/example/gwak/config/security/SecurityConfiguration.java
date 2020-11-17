1f0917fdd62a5e67017f4b76fbcf53862df26f39
gmldnjs26 <imleesky265@gmail.com>
2020-11-17 23:35:50 +0900

    Get PasswordPattern from Server

--- src/main/java/com/example/gwak/config/security/SecurityConfiguration.java
+++ src/main/java/com/example/gwak/config/security/SecurityConfiguration.java
@@ -29,7 +29,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // jwt token으로 인증하므로 세션은 필요없으므로 생성안함.
 		.and()
 			.authorizeRequests() // 다음 리퀘스트에 대한 사용권한 체크
-			.antMatchers("/api/login","/api/join","/api/selectTil").permitAll() // 가입 및 인증 주소는 누구나 접근가능
+			.antMatchers("/api/login","/api/join","/api/selectTil","/api/pwdPattern").permitAll() // 가입 및 인증 주소는 누구나 접근가능
 			.anyRequest().hasRole("USER") // 그외 나머지 요청은 모두 인증된 회원만 접근 가능
 		.and()
 			// jwt token 필터를 id/password 인증 필터 전에 넣는다
--- src/main/java/com/example/gwak/controller/MemberController.java
+++ src/main/java/com/example/gwak/controller/MemberController.java
@@ -1,14 +1,12 @@
 package com.example.gwak.controller;
 
-import java.util.Objects;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.gwak.dto.AuthDto;
 import com.example.gwak.dto.Member;
 import com.example.gwak.service.MemberService;
 
@@ -35,4 +33,8 @@ public class MemberController {
 		return ResponseEntity.ok(memberService.login(member));
 	}
 	
+	@GetMapping("/api/pwdPattern")
+	public ResponseEntity<Object> passwordPattern() {
+		return ResponseEntity.ok("/^(([^<>()[\\]\\\\.,;:\\s@\"]+(\\.[^<>()[\\]\\\\.,;:\\s@\"]+)*)|(\".+\"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$/");
+	}
 }
